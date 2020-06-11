40a1edfd402473b5a189ca042bc6c7a2f364dc41
Christiandv <christian.david.vaughan@gmail.com>
2020-06-11 14:02:05 -0400

    Hey it's tron now!
    Like the whole game of tron is there

--- public/sketch.js
+++ public/sketch.js
@@ -1,36 +1,115 @@
 var socket;
 
+
+var id = 0;
+var started = false; 
+var hasLost = false;
+var hasWon = false;
+var readied = false;
+
 function setup(){
     createCanvas(500,500);
     background(51);
 
     // actually connect from client side, need to update when not running locally
     socket = io.connect('http://127.0.0.1:3000');
-    socket.on('mouse', newDrawing);
+    socket.on('reset', reset);
+    socket.on('playerLocUpdate', drawPlayers);
+    socket.on('start', start);
+    socket.on('youLost', lost);
+    socket.on('youWin', win);
 }
 
-function newDrawing(data){
-    noStroke();
-    fill(200);
-    ellipse(data.x, data.y, 50,50);
+function reset(data){
+    console.log("was given id: " + id);
+    id = data.id;
+    var started = false; 
+    var hasLost = false;
+    var hasWon = false;
+    var readied = false;
+    background(51);
 }
+
+
+// draws the new location of a player to the screen
+// expects: color, x, y,
+function drawPlayers(data){
+    noStroke();
+    var size= 0;
+    for(var i = 0; i < data.length; i ++){
+        fill(data[i].color.r,data[i].color.g,data[i].color.b);
+      
+        square(data[i].x,data[i].y,data[i].size);
+        console.log("drawing");
+    }
+}
+
+
 
 function draw(){
-  
+    if(started){
+        if(hasLost){
+            // draw a you lose banner
+            fill(255);
+            textSize(50);
+            text("You Lose!",100,100);
+        }else if(hasWon){
+            //draw a you win banner
+            fill(255);
+            textSize(50);
+            text("You Win!",100,100);
+        }
+    }else{
+        if(readied){
+            // game will start soon banner
+        }else{
+            // please press any button so say you are ready
+        }
+    }
+ 
 }
 
-function mouseDragged(){
-    console.log(mouseX + ", " + mouseY);
-
-    // the data we want to send to the server
-    var data ={
-        x:mouseX,
-        y:mouseY
+function keyPressed() {
+    if(started && !hasLost && !hasWon){
+        console.log("sending input");
+        if (keyCode === LEFT_ARROW) {
+            var direction = 2;
+            
+        } else if (keyCode === RIGHT_ARROW) {
+            var direction = 0;
+           
+        } else if (keyCode === UP_ARROW) {
+            var direction = 1;
+            
+        }else if (keyCode === DOWN_ARROW) {
+            var direction = 3;
+            
+        }
+        var data = {
+            dir:direction,
+            id:id
+        }
+        socket.emit('input', data);
+    }else if(!readied){
+        socket.emit('ready');
+        console.log("sending ready");
     }
-    // send message with mouse label and needed data
-    socket.emit('mouse',data);
-    noStroke();
-    fill(200);
-    ellipse(mouseX, mouseY, 50,50);
+    
+}
+
+
+function win(){
+    hasWon = true;
+}
+
+function lost(){
+    hasLost = true;
+}
+
+function start(){
+    background(51);
+    // make a banner that says start?
+    console.log("START");
+    started = true;
 }
 
--- server.js
+++ server.js
@@ -1,3 +1,13 @@
+class Wall{
+    // make this actual rectangles so that we can cut down the number of walls
+    constructor(x, y, width, height){
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+}
+
 var express = require('express'); // the express library
 var socket = require('socket.io'); // everything for the socket library
 
@@ -12,22 +22,231 @@ console.log("My socket server is running");
 
 var io = socket(server);
 
+var Connections = [];
+var numPlayers = 0;
+var readyPlayers = 0;
+var stillAlivePlayers = 0;
+let speed = 3;
+var gameOver = false;
+var startingLocations = [
+    {x:100, y:100, dir:0},
+    {x:400, y:100, dir:3},
+    {x:100, y:400, dir:1},
+    {x:400, y:400, dir:2}];
+var maxPlayers = 4;
+
+var walls = [];
+
+
 // handle the event that someone connects
 io.sockets.on('connection', newConnection);
 
+//starts the game loop
+setInterval(update, 33);
 
 // handle new connection being made given socket 
 function newConnection(socket){
-    console.log('new connection' + socket.id);
+    console.log('new connection: ' + socket.id);
+    
 
-    // whenever we recieve a 'mouse' message do mouseMsg
-    socket.on('mouse', mouseMsg);
+    thisPlayer = new Player(startingLocations[numPlayers].x,
+        startingLocations[numPlayers].y,
+        startingLocations[numPlayers].dir,
+        new Color( Math.floor(Math.random() * 255),
+        Math.floor(Math.random() * 255),
+        Math.floor(Math.random() * 255)));
+       
+    var node = {
+        id: socket.id,
+        socket: socket,
+        player: thisPlayer
+    };
+    
+    Connections[numPlayers] = node;
+  
+    var data = {
+        id: numPlayers
+    }
+    // tell the client to reset + give it its id
+    socket.emit('reset', data)
 
-    function mouseMsg(data){
-        // emits to everyone else
-        socket.broadcast.emit('mouse', data);
-        //io.sockets.emit('mouse', data); would also send back to this one
+    numPlayers++;
+
+    socket.on('input', handleInput);
+
+    function handleInput(data){
+        // Make this better by using a has map with the socket id as the key
+        Connections[data.id].player.turn(data.dir);
+    }
+
+    socket.on('ready', playerReadied);
+    function playerReadied(){
+        console.log("player readied");
+        readyPlayers ++;
+        if(readyPlayers== numPlayers){
+            stillAlivePlayers = numPlayers;
+            gameOver = false;
+            walls = [];
+            walls.push(new Wall(0,-10,500,10));
+            walls.push(new Wall(0, 500,500,10));
+            walls.push(new Wall(-10,0,10,500));
+            walls.push(new Wall(500,0,10,500));
+            io.sockets.emit('start');
+        }
+    }
+
+    // handle what we do if a client disconnects
+    socket.on('disconnect', function() {
+        console.log('Client has disconnected');
+    });
+}
+
+
+
+class Color{
+    constructor(r,g,b){
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+
+   
+    
+    packageUp(){
+        return {r:this.r,
+            g:this.g,
+            b:this.b};
     }
 }
 
+// theserver keeps track of all player info
+
+class Player{
+    
+    constructor(x, y, direction, color) {
+        this.hasLost = false;
+        this.color = color;
+        this.x = x;
+        this.y = y;
+        switch(direction){
+            case 0:
+                this.xspeed = speed;
+                this.yspeed = 0;
+                break;
+            case 1:
+                this.xspeed = 0;
+                this.yspeed = -1 * speed;
+                break;
+            case 2:
+                this.xspeed = -1 * speed;
+                this.yspeed = 0;
+                break;
+            case 3:
+                this.xspeed = 0;
+                this.yspeed = speed;
+                break;
+        }
+        this.size = 3; // radius of player 
+        // we are drawing players like normal rectangles, from top left
+
+    }
+
+    // expects a wall to be given to test against
+    collide(wall){
+        if (this.x  < wall.x + wall.width &&
+            this.x + this.size > wall.x &&
+            this.y < wall.y + wall.height &&
+            this.y + this.size > wall.y) {
+             // collision detected!
+             return true;
+         }
+         return false;
+    }
+    
+    move() {
+        this.x += this.xspeed;
+        this.y += this.yspeed;
+    }
+
+    turn(direction){
+        switch(direction){
+            case 0:
+                if(this.xspeed == 0){
+                    this.xspeed = speed;
+                    this.yspeed = 0;
+                }
+                break;
+            case 1:
+                if(this.yspeed == 0){
+                    this.xspeed = 0;
+                    this.yspeed = -1 * speed;
+                }
+                break;
+            case 2:
+                if(this.xspeed == 0){
+                    this.xspeed = -1 * speed;
+                    this.yspeed = 0;
+                }
+                break;
+            case 3:
+                if(this.yspeed == 0){
+                    this.xspeed = 0;
+                    this.yspeed = speed;
+                }
+                break;
+        }
+    }
+
+}
+
+// update loop. need to actually check that players lost
+function update(){
+    if(readyPlayers == numPlayers && numPlayers != 0 && !gameOver){
+        var data = [];
+        for(let i = 0; i < numPlayers; i++){
+            if(!Connections[i].player.hasLost){
+                Connections[i].player.move();
+                // check collisions
+                for(var j = 0; j < walls.length; j++){
+                    if(Connections[i].player.collide(walls[j])){
+                        Connections[i].player.hasLost = true;
+                        //Connections[i].socket.emit('youLost');
+                        io.to(Connections[i].id).emit('youLost');
+                        stillAlivePlayers --;
+                    }
+                }
+                walls.push(new Wall(Connections[i].player.x,
+                    Connections[i].player.y,
+                    Connections[i].player.size,
+                    Connections[i].player.size));
+                var p = {
+                    x:Connections[i].player.x,
+                    y:Connections[i].player.y,
+                    size: Connections[i].player.size,
+                    color: Connections[i].player.color.packageUp()
+                };
+                console.log(p.x + " " + p.y);
+                data.push(p);
+            }
+        }
+        io.sockets.emit('playerLocUpdate',data);
+        if(stillAlivePlayers < 2){
+            gameOver = true;
+            //game is over, either a tie or winner
+            if(stillAlivePlayers == 1){
+                // we have a winner, tell them
+                for(let i = 0; i < numPlayers; i++){
+                    if(!Connections[i].player.hasLost){
+                        // this is the winner
+                        io.to(Connections[i].id).emit('youWin');
+                    }else{
+                        // tell the others who won?
+                    }
+                }
+            }else if (stillAlivePlayers == 0){
+                // we have a tie
+            }
+        }
+    }
+}
 
